0f05a1436d883cc17bd84ed8c5eb33c215c4546c
naybyal <officiallynabiel@gmail.com>
2024-08-10 01:36:20 +0530

    codecrafters submit [skip ci]

--- src/parser.rs
+++ src/parser.rs
@@ -48,6 +48,29 @@ pub fn parse(file_contents: &str) -> i32 {
                     println!("{}.0", number);
                 }
             },
+            'a'..='z' | 'A'..='Z' | '_' => {
+                let mut identifier = String::new();
+                while let Some(&next_char) = chars.peek() {
+                    if next_char.is_alphanumeric() || next_char == '_' {
+                        identifier.push(next_char);
+                        chars.next();
+                    } else {
+                        break;
+                    }
+                }
+
+                let keywords = [
+                    "and", "class", "else", "false", "for", "fun", "if", "nil", "or",
+                    "print", "return", "super", "this", "true", "var", "while"
+                ];
+
+                if keywords.contains(&identifier.as_str()) {
+                    let keyword = identifier.to_uppercase();
+                    println!("{}", keyword);
+                } else {
+                    println!("{}", identifier);
+                }   
+            },
             _ => {
                 println!("EOF null");
                 result = 0;
