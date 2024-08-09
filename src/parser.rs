769d158c5a60cb6960ddfa15b19d7f52c24c8ada
naybyal <officiallynabiel@gmail.com>
2024-08-10 01:45:50 +0530

    codecrafters submit [skip ci]

--- src/parser.rs
+++ src/parser.rs
@@ -56,7 +56,7 @@ pub fn parse(file_contents: &str) -> i32 {
                 while let Some(&next_char) = chars.peek() {
                     if next_char == '\"' {
                         chars.next();
-                        println!("\"{string}\"");
+                        println!("{string}");
                         unterminated = false;
                         break;
                     }
