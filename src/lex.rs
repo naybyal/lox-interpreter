6e823198a3b06d8b437b6299f374f4ffd85b7fad
naybyal <officiallynabiel@gmail.com>
2024-08-10 00:03:21 +0530

    codecrafters submit [skip ci]

--- src/lex.rs
+++ src/lex.rs
@@ -167,7 +167,7 @@ pub fn tokenize(file_contents: &str) -> i32 {
                 if is_float {
                     if number.ends_with('0') {
                         number.pop();
-                        println!("NUMBER {}0 {}0", number, number);
+                        println!("NUMBER {}0 {}", number, number);
                     } else if number.ends_with('.') {
                         number.pop();
                         println!("NUMBER {} {}.0", number, number);
