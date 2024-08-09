c9e9fcd35d1a2c333454285e53c0f6602842e1b4
naybyal <officiallynabiel@gmail.com>
2024-08-10 00:28:42 +0530

    codecrafters submit [skip ci]

--- src/eval.rs
+++ src/eval.rs
@@ -2,23 +2,24 @@ pub fn evaluate(file_contents: &str) -> i32 {
     let mut lines = 1;
     let mut chars = file_contents.chars().peekable();
 
-    while let Some(&char = chars.peek()) {
-        match character {
-            'a'..='z' | 'A'..='Z' => {
-                let literals = [
-                    'true', 'false', 'nil'
-                ];
-                let mut identifier = String::new();
-                while let Some(&char) = chars.peek() {
-                    if char.is_alphanumeric() {
-                        identifier.push(char);
-                        chars.next();
-                    } else {
-                        break;
-                    }
-                }
+    // while let Some(&char = chars.peek()) {
+    //     match character {
+    //         'a'..='z' | 'A'..='Z' => {
+    //             let literals = [
+    //                 'true', 'false', 'nil'
+    //             ];
+    //             let mut identifier = String::new();
+    //             while let Some(&char) = chars.peek() {
+    //                 if char.is_alphanumeric() {
+    //                     identifier.push(char);
+    //                     chars.next();
+    //                 } else {
+    //                     break;
+    //                 }
+    //             }
                 
-            },
-        }
-    }
+    //         },
+    //     }
+    // }
+    0
 }
