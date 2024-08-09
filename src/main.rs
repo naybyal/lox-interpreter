30b56a4c3827d654c86314930784b8587e53bbdd
naybyal <officiallynabiel@gmail.com>
2024-08-10 00:27:17 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -53,21 +53,21 @@ fn main() {
                 println!("EOF  null");
             }
         },
-        "evaluate" => {
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
+        // "evaluate" => {
+        //     writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
 
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
+        //     let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+        //         writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+        //         String::new()
+        //     });
 
-            if !file_contents.is_empty() {
-                let result = eval::evaluate(&file_contents);
-                exit(result);
-            } else {
-                println!("EOF  null");
-            }
-        },
+        //     if !file_contents.is_empty() {
+        //         let result = eval::evaluate(&file_contents);
+        //         exit(result);
+        //     } else {
+        //         println!("EOF  null");
+        //     }
+        // },
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             return;
