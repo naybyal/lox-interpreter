9e31f768f5b237d239f1c4053b8fd5f500781dc3
naybyal <officiallynabiel@gmail.com>
2024-08-10 02:55:45 +0530

    codecrafters submit [skip ci]

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,11 @@
 pub fn parse(file_contents: &str) -> i32 {
-    let mut result = 0;
-    let mut chars = file_contents.chars().peekable();
+    let mut result: i32 = 0;
+    let mut chars: std::iter::Peekable<std::str::Chars> = file_contents.chars().peekable();
 
     while let Some(&char) = chars.peek() {
         match char {
             'a'..='z' | 'A'..='Z' => {
-                let mut identifier = String::new();
+                let mut identifier: String = String::new();
                 while let Some(&char) = chars.peek() {
                     if char.is_alphanumeric() {
                         identifier.push(char);
@@ -14,14 +14,14 @@ pub fn parse(file_contents: &str) -> i32 {
                         break;
                     }
                 }
-                let literals = ["true", "false", "nil"];
+                let literals: [&str; 3] = ["true", "false", "nil"];
                 if literals.contains(&identifier.as_str()) {
                     println!("{identifier}");
                 }
             },
             '0'..='9' => {
-                let mut number = String::new();
-                let mut is_float = false;
+                let mut number: String = String::new();
+                let mut is_float: bool = false;
                 while let Some(&next_char) = chars.peek() {
                     if next_char.is_digit(10) {
                         number.push(next_char);
@@ -50,8 +50,8 @@ pub fn parse(file_contents: &str) -> i32 {
             },
             '\"' => {
                 chars.next();
-                let mut string = String::new();
-                let mut unterminated = true;
+                let mut string: String = String::new();
+                let mut unterminated: bool = true;
 
                 while let Some(&next_char) = chars.peek() {
                     if next_char == '\"' {
@@ -66,6 +66,28 @@ pub fn parse(file_contents: &str) -> i32 {
                 }
 
                 if unterminated {
+                    result = 65;
+                }
+            },
+            '(' => {
+                chars.next();
+                let mut string: String = String::new();
+                let mut unterminated: bool = true;
+
+                while let Some(&next_char) = chars.peek() {
+                    if next_char == ')' {
+                        chars.next();
+                        println!("\"{}\"", string);
+                        unterminated = false;
+                        break;
+                    }
+                    
+                    string.push(next_char);
+                    chars.next();
+                }
+
+                if unterminated {
+                    eprintln!("Error: Unterminated string.");
                     result = 65;
                 }
             },
